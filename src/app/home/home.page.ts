70b26fe251e478fb6804ef7482c82aa753992118
VICTUS-15\andre <andrewportillo5530@gmail.com>
2024-09-29 16:32:58 -0600

    se creo el contenedor del banner de promocion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,15 +10,12 @@ export class HomePage {
   constructor() {}
 
   isMenuOpen = false; // Variable para manejar el estado del menú
-  searchTerm: string = '';
+  
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen; // Alternar el estado del menú
   }
 
-  onSearch(event: any) {
-    this.searchTerm = event.target.value;
-    // Aquí puedes implementar la lógica para filtrar o buscar elementos
-  }
+
 
 }
