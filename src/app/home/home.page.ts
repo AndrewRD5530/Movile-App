25ae9f81d24ede7fce08208b2c367e3ee305af28
VICTUS-15\andre <andrewportillo5530@gmail.com>
2024-09-29 15:47:08 -0600

    se creo el menu de navegacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,10 @@ export class HomePage {
 
   constructor() {}
 
+  isMenuOpen = false; // Variable para manejar el estado del menú
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Alternar el estado del menú
+  }
+
 }
