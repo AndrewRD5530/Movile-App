341e1102f49a5815f15f20234752bac71b8e75d6
Adrian-Aguilera <cruz74270@gmail.com>
2024-10-08 17:49:41 -0600

    fix: buscador con alerta y barra de carga

--- src/app/buscador/buscador.page.ts
+++ src/app/buscador/buscador.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 import { debounceTime } from 'rxjs/operators';
+import { AlertController } from '@ionic/angular';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -14,27 +15,25 @@ export class BuscadorPage implements OnInit {
   isMenuOpen = false; // Estado del menú
   isLoading = false;  // Estado de la barra de progreso
   isAlertOpen = false;  // Estado de la alerta
-  private searchSubject: Subject<string> = new Subject(); // Para manejar debounce en búsqueda
+  private searchSubject: Subject<string> = new Subject();
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
   ngOnInit() {
-    // Configurar el debounce para evitar múltiples solicitudes
     this.searchSubject.pipe(debounceTime(500)).subscribe(searchTerm => {
       this.getProducts(searchTerm);
     });
   }
 
   toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen; // Alternar el estado del menú
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
-  // Método para manejar el input de búsqueda
   searchProducts(event: any) {
     const searchTerm = event.target.value.trim();
     if (searchTerm) {
       this.isLoading = true; // Mostrar la barra de progreso mientras se busca
-      this.searchSubject.next(searchTerm); // Emitir el término de búsqueda después del debounce
+      this.searchSubject.next(searchTerm);
     } else {
       this.products = []; // Limpiar los productos si no hay búsqueda
     }
@@ -42,7 +41,6 @@ export class BuscadorPage implements OnInit {
 
   async getProducts(searchTerm: string) {
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzI5NTY2NDQzLCJpYXQiOjE3MjY5NzQ0NDMsImp0aSI6IjE2YTFmZjA0MjZkZDQzMDA4NTk1OTdhN2MzZWU0MjcyIiwidXNlcl9pZCI6Mn0.be7ZN6uS-UfO73XEaZy3bPtExryonp-Uv_vrnnc5QAI';
-
     const headers = {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
@@ -58,33 +56,29 @@ export class BuscadorPage implements OnInit {
       const response = await axios.post(url, json, { headers });
       const productos = response.data.data.productos;
 
-      // Verifica si no hay productos encontrados
       if (productos.length > 0 && productos[0].details === "producto no encontrado") {
-        this.showNoProductsFoundAlert();
+        await this.showAlert('No se encontraron productos');
       } else {
-        this.products = productos.flat(); // Aplana el array si es necesario
-        this.isAlertOpen = false;
+        this.products = productos.flat();
       }
     } catch (error) {
       console.error("Error al obtener productos:", error);
-      this.showErrorAlert();
+      await this.showAlert('Ocurrió un error al buscar productos.');
     } finally {
-      this.isLoading = false; // Asegura que siempre se oculta el indicador de carga
+      this.isLoading = false;
     }
   }
 
-  // Método para mostrar una alerta si no se encontraron productos
-  showNoProductsFoundAlert() {
-    this.isAlertOpen = true;
-    this.products = [];
-    console.log("Producto no encontrado");
-  }
+  // Método para mostrar la alerta
+  async showAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Atención',
+      message: message,
+      buttons: ['OK'],
+      cssClass: 'custom-alert'
+    });
 
-  // Método para mostrar una alerta en caso de error
-  showErrorAlert() {
-    this.isAlertOpen = true;
-    this.products = [];
-    console.log("Error al buscar productos");
+    await alert.present();
   }
 
 }
