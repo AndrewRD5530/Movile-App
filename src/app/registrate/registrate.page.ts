f71d6a88d059a8a2c8de830a3518cac198cfe0d0
Adrian-Aguilera <cruz74270@gmail.com>
2024-10-08 19:13:35 -0600

    feat: funcionalidad de crear cuenta y de hacer login

--- src/app/registrate/registrate.page.ts
+++ src/app/registrate/registrate.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-registrate',
   templateUrl: './registrate.page.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistratePage implements OnInit {
 
+  email: string = '';
+  password: string = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+  async onSubmit() {
+    console.log("Email:", this.email);
+    console.log("Contraseña:", this.password);
+    // Aquí se puede realizar la validación de la contraseña y enviar la solicitud
+    const url = 'http://127.0.0.1:8000/api/usuario/crear';
+    const Credenciales = {
+      "correo": this.email,
+      "password": this.password
+    };
+    try {
+      const response = await axios.post(url, Credenciales);
+      const respuesta = response.data;
+      if (respuesta.data.details) {
+        console.log("Nuevo ususario usuario:", respuesta.data.details.details);
+        //redireccionar a la página de inicio
+        window.location.href = '/login';
+      } else {
+        console.log("Error:", respuesta.error);
+      }
+    } catch (error) {
+      console.error("Error al enviar solicitud:", error);
+    }
+  }
 }
